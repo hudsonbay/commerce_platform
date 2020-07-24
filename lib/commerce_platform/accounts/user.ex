9f1043e2a423c8bb075a93014432ae039deb88c8
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-07-24 19:09:10 -0400

    added new fields to user

--- lib/commerce_platform/accounts/user.ex
+++ lib/commerce_platform/accounts/user.ex
@@ -2,6 +2,8 @@ defmodule CommercePlatform.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias CommercePlatform.World.Country
+
   schema "users" do
     field :email, :string, unique: true
     field :first_name, :string
@@ -10,6 +12,23 @@ defmodule CommercePlatform.Accounts.User do
     field :password, :string, virtual: true
     field :password_confirmation, :string, virtual: true
     field :role, :string, default: "user"
+
+    field :phone, :string
+    field :shipping_address, :string
+    field :shipping_city, :string
+    field :shipping_region, :string
+    field :shipping_state, :string
+    field :shipping_postal_code, :string
+
+    field :billing_addres, :string
+    field :billing_city, :string
+    field :billing_region, :string
+    field :billing_state, :string
+    field :billing_postal_code, :string
+
+    # TODO: add these fields credit_card, credit_card_type_id, shipping_country_id belongs_to :country, Country, field :shipping_country, :string, 
+    # field :billing_country, :string
+    # TODO: see if there's gonna be an address 1 and address 2, and subsequently a default address
 
     timestamps()
   end
---
+++ priv/repo/migrations/20200724225909_add_new_fields_to_user.exs
@@ -0,0 +1,19 @@
+defmodule CommercePlatform.Repo.Migrations.AddNewaddsToUser do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :phone, :string
+      add :shipping_address, :string
+      add :shipping_city, :string
+      add :shipping_region, :string
+      add :shipping_state, :string
+      add :shipping_postal_code, :string
+      add :billing_addres, :string
+      add :billing_city, :string
+      add :billing_region, :string
+      add :billing_state, :string
+      add :billing_postal_code, :string
+    end
+  end
+end
