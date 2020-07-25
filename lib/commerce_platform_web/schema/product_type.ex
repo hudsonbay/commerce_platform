7fafa337af77dec33987f5db82ca290523c18e02
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-07-24 23:02:36 -0400

    modified product_type

--- lib/commerce_platform_web/schema/product_type.ex
+++ lib/commerce_platform_web/schema/product_type.ex
@@ -5,10 +5,7 @@ defmodule CommercePlatformWeb.Schema.Types.ProductType do
     field(:id, :id)
     field(:name, :string)
     field(:price, :float)
-<<<<<<< HEAD
     field(:stock, :integer)
-=======
->>>>>>> feature/implementing_graphql_operations_with_products
     field(:description, :string)
     field(:size, :float)
     field(:weight, :float)
@@ -19,10 +16,7 @@ defmodule CommercePlatformWeb.Schema.Types.ProductType do
   input_object :product_input_type do
     field(:name, non_null(:string))
     field(:price, non_null(:float))
-<<<<<<< HEAD
     field(:stock, non_null(:integer))
-=======
->>>>>>> feature/implementing_graphql_operations_with_products
     field(:description, non_null(:string))
     field(:size, non_null(:float))
     field(:weight, non_null(:float))
