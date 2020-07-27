58a3607866f275fd86907608c9c02035c171a560
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-07-27 01:47:00 -0400

    added a test for products query

---
+++ test/commerce_platform_web/schema/query/product_test.exs
@@ -0,0 +1,30 @@
+defmodule CommercePlatform.Schema.Query.ProductTest do
+  use CommercePlatform.ConnCase, async: true
+
+  setup do
+    CommercePlatform.Seeds.run()
+  end
+
+  @query """
+  {
+      products {
+        name
+      }
+  }
+  """
+
+  test "products field returns products" do
+    conn = build_conn()
+    conn = get conn, "/api/v1/graphql", query: @query
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "products" => [
+                 %{"name" => "Meat"},
+                 %{"name" => "Bread"},
+                 %{"name" => "Pizza"}
+               ]
+             }
+           }
+  end
+end
