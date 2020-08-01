3cf76d4b99635b93b18050be663b39db1ed24994
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-08-01 04:50:27 -0400

    fixed typo on permissions migration

--- priv/repo/migrations/20200801084515_create_permissions.exs
+++ priv/repo/migrations/20200801084515_create_permissions.exs
@@ -5,7 +5,7 @@ defmodule CommercePlatform.Repo.Migrations.CreatePermissions do
     create table(:permissions) do
       add :name, :string
       add :action, :string
-      add :role_id, references(:role, on_delete: :delete_all)
+      add :role_id, references(:roles, on_delete: :delete_all)
 
       timestamps()
     end
