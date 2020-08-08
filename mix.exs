b4d8376eaa3185359aa658d62456748a005ece35
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-08-08 04:41:44 -0400

    Added dataloader to mix

--- mix.exs
+++ mix.exs
@@ -52,6 +52,7 @@ defmodule CommercePlatform.MixProject do
       {:absinthe, "~> 1.4.0"},
       {:absinthe_plug, "~> 1.4.0"},
       {:absinthe_ecto, "~> 0.1.3"},
+      {:dataloader, "~> 1.0.0"},
       {:guardian, "~> 2.0"},
       {:timex, "~> 3.5"},
       {:pdf_generator, "~> 0.6.2"},
