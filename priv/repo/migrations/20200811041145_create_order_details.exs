bf001f382fd003c02bcc5b3eca5581283fc3f296
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-08-11 00:49:46 -0400

    created oreder_details migration

---
+++ priv/repo/migrations/20200811041145_create_order_details.exs
@@ -0,0 +1,18 @@
+defmodule CommercePlatform.Repo.Migrations.CreateOrderDetails do
+  use Ecto.Migration
+
+  def change do
+    create table(:order_details) do
+      add :discount, :float
+      add :unit_price, :float
+      add :quantity, :integer
+      add :total, :float
+      add :order_id, references(:orders, on_delete: :delete_all)
+      add :product_id, references(:products, on_delete: :delete_all)
+
+      timestamps()
+    end
+
+    create index(:order_details, [:order_id, :product_id])
+  end
+end
