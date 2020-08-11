83454a84df7fad60b0c4089d732f36c93b2564e8
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-08-11 03:04:59 -0400

    deleted unused migrations

--- priv/repo/migrations/20200804082821_add_orderstates_to_orders.exs
+++
@@ -1,9 +0,0 @@
-defmodule CommercePlatform.Repo.Migrations.AddOrderstatesToOrders do
-  use Ecto.Migration
-
-  def change do
-    alter table(:orders) do
-      add :order_state_id, references(:order_state)
-    end
-  end
-end
