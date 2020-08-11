a3d0ba472224925588c11ce05b078a4edd8fad1f
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-08-11 03:06:50 -0400

    created order status schema

---
+++ lib/commerce_platform/orders/order_status.ex
@@ -0,0 +1,22 @@
+defmodule CommercePlatform.Orders.OrderStatus do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias CommercePlatform.Orders.Order
+
+  schema "order_status" do
+    field :description, :string
+    field :name, :string
+
+    has_many :orders, Order
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(order_state, attrs) do
+    order_state
+    |> cast(attrs, [:name, :description])
+    |> validate_required([:name, :description])
+  end
+end
