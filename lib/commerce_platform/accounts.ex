8013264a77c5662116a49a60cc478c52b513ba09
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-08-20 02:16:58 -0400

    User schema can preload related data

--- lib/commerce_platform/accounts.ex
+++ lib/commerce_platform/accounts.ex
@@ -6,7 +6,7 @@ defmodule CommercePlatform.Accounts do
   import Ecto.Query, warn: false
   alias CommercePlatform.Repo
 
-  alias CommercePlatform.Accounts.User
+  alias CommercePlatform.Accounts.{User, MembershipType, ShippingAddress}
 
   @doc """
   Returns the list of users.
@@ -18,7 +18,10 @@ defmodule CommercePlatform.Accounts do
 
   """
   def list_users do
-    Repo.all(User)
+    User
+    |> Repo.all()
+    |> Repo.preload(:membership_type)
+    |> Repo.preload(:shipping_addresses)
   end
 
   @doc """
@@ -35,7 +38,12 @@ defmodule CommercePlatform.Accounts do
       ** (Ecto.NoResultsError)
 
   """
-  def get_user!(id), do: Repo.get!(User, id)
+  def get_user!(id) do
+    User
+    |> Repo.get!(id)
+    |> Repo.preload(:membership_type)
+    |> Repo.preload(:shipping_addresses)
+  end
 
   @doc """
   Creates a user.
@@ -52,6 +60,7 @@ defmodule CommercePlatform.Accounts do
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:membership_type, with: &MembershipType.changeset/2)
     |> Repo.insert()
   end
 
@@ -70,6 +79,7 @@ defmodule CommercePlatform.Accounts do
   def update_user(%User{} = user, attrs) do
     user
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:membership_type, with: &MembershipType.changeset/2)
     |> Repo.update()
   end
 
@@ -101,8 +111,6 @@ defmodule CommercePlatform.Accounts do
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
   end
-
-  alias CommercePlatform.Accounts.MembershipType
 
   @doc """
   Returns the list of membership_types.
@@ -389,8 +397,6 @@ defmodule CommercePlatform.Accounts do
   def change_permission(%Permission{} = permission, attrs \\ %{}) do
     Permission.changeset(permission, attrs)
   end
-
-  alias CommercePlatform.Accounts.ShippingAddress
 
   def list_shipping_addresses_by_user(id) do
     from(sa in ShippingAddress,
